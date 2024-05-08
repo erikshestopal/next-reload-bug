83ac5b466eea291a444118b62a8ee0c21e2bd909
Erik Shestopal <shestopalerik@gmail.com>
2024-05-08 16:57:36 -0400

    fi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,7 @@
-import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
-
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body>
-        <Theme>{children}</Theme>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
